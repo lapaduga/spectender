6b5a72fc0ada43c86c776703eb2d64d93c86c745
lapaduga <deniskotov29042015@gmail.com>
2020-10-25 04:03:35 +0500

    form&header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,87 @@
-$(document).ready(function () {
+"use strict"
 
+document.addEventListener('DOMContentLoaded', function () {
+	const headerButton = document.querySelector('.header__button');
+	const body = document.body;
+	const callback = document.querySelector('#callback');
+	const close = document.querySelector('.popup__close');
+	const popupArea = document.querySelector('.popup__area');
+	const form = document.getElementById('form');
+	const alert = document.querySelector('.popup__alert');
+
+	form.addEventListener('submit', formSend);
+
+	headerButton.onclick = function(){
+		callback.classList.toggle('active');
+		body.classList.toggle('lock');
+	}
+	close.onclick = function(){
+		callback.classList.toggle('active');
+		body.classList.toggle('lock');
+	}
+	popupArea.onclick = function(){
+		callback.classList.toggle('active');
+		body.classList.toggle('lock');
+	}
+
+	async function formSend(e){
+		e.preventDefault();
+
+		let error = formValidate(form);
+
+		let formData = new FormData(form);
+
+		if(error===0){
+			alert.classList.remove('active');
+			form.reset();
+		} else{
+			alert.classList.add('active');
+		}
+	}
+
+	function formValidate(form) {
+		let error = 0;
+		let formReq = document.querySelectorAll('._req');
+
+		for (let index = 0; index < formReq.length; index++) {
+			const input = formReq[index];
+			formRemoveError(input);
+
+			if(input.value === ''){
+				formAddError(input);
+				error++;
+			}
+		}
+		return error;
+	}
+
+	function formAddError(input) {
+		input.parentElement.classList.add('_error');
+		input.classList.add('_error');
+	}
+
+	function formRemoveError(input) {
+		input.parentElement.classList.remove('_error');
+		input.classList.remove('_error');
+	}
+
+	//header height on scroll behaviour
+
+	window.onscroll = function () { scrollFunction() };
+	function scrollFunction() {
+			if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+				document.getElementById("header").style.backgroundColor = "#fff";
+			} else {
+				document.getElementById("header").style.backgroundColor = "transparent";
+			}
+		if (document.documentElement.clientWidth > 992) {
+			if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+				document.getElementById("header").style.paddingTop = "10px";
+				document.getElementById("header").style.paddingBottom = "10px";
+			} else {
+				document.getElementById("header").style.paddingTop = "60px";
+				document.getElementById("header").style.paddingBottom = "0px";
+			}
+		}
+	}
 });
